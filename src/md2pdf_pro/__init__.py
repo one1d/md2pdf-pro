2ade5d57f53b19113cc7869168084c6c0ba49c6a
1diot <19486675+one1d@users.noreply.github.com>
2026-02-28 17:47:39 +0800

    chore: update author to Guoqin Chen

--- src/md2pdf_pro/__init__.py
+++ src/md2pdf_pro/__init__.py
@@ -1,7 +1,7 @@
 """MD2PDF Pro - Batch Markdown to PDF Converter."""
 
 __version__ = "1.0.1"
-__author__ = "MD2PDF Team"
+__author__ = "Guoqin Chen"
 
 from md2pdf_pro.config import ProjectConfig
 from md2pdf_pro.converter import PandocEngine
